609e85ee8ecbd405056037e0ac88340c75c2c5cd
김지민 <samjin123kr@gmail.com>
2022-01-20 23:30:54 +0900

    [서버/김지민] 인텔리J commit

--- src/main/java/classpackage/Child.java
+++ src/main/java/classpackage/Child.java
@@ -1,4 +1,15 @@
 package classpackage;
 
-public class Child {
+public class Child
+{
+    public Child()
+    {
+        //그냥 공부중
+    }
+
+    public boolean ChildPeople()
+    {
+        return true;
+    }
+
 }
--- src/main/java/hello/JavaStudy/JavaStudyApplication.java
+++ src/main/java/hello/JavaStudy/JavaStudyApplication.java
@@ -1,12 +1,17 @@
 package hello.JavaStudy;
 
+import classpackage.Child;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaStudyApplication {
-
-	public static void main(String[] args) {
+public class JavaStudyApplication
+{
+	public static void main(String[] args)
+	{
+		Child a = new Child();
+		boolean booltype = a.ChildPeople();
+		System.out.println(booltype);
 		SpringApplication.run(JavaStudyApplication.class, args);
 	}
 
