cf827214da5d9ce6b25ac9e723f15366e70d6dbf
김지민 <samjin123kr@gmail.com>
2022-01-29 21:22:39 +0900

    [Study] Java 초기화 블럭에 대해서

--- src/main/java/hello/JavaStudy/StandardJava/StandardJava.java
+++ src/main/java/hello/JavaStudy/StandardJava/StandardJava.java
@@ -1,14 +1,32 @@
 package hello.JavaStudy.StandardJava;
 
 public class StandardJava {
+    int a ;
+    int b;
+    static int c;
 
+    static { c = 20; }
+    { a =20; b=30; }
     public StandardJava()
     {
+
+
 
     }
 
     public void Update()
     {
+        try
+        {
+            int a =20;
+        }
+        catch (Exception ex)
+        {
+
+        }
+        int b =20;
+        System.out.println(b);
+
         return;
     }
 }
