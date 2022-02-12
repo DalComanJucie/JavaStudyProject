d704b4f276e80042c71ccd32ce04f106afc319ef
DalComanJucie <samjin123kr@gmail.com>
2022-02-12 22:20:49 +0900

    [Study] 의존성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -11,10 +12,11 @@ import hello.core.order.OrderServiceImpl;
 
 //생성자를 통해서 주입한다.
 //의존관계 주입한다 Dependency Injection
-//디자인 패턴중 생성 패턴이네... 설정파일 이라는거잖아 ... ㄷ
+//디자인 패턴중 생성 패턴이네... 설정파일 이라는거잖아 ... ㄷ -> // App Config 를 통해서 사용영역과 구성 영역을 나누어 버렸다.
+//구성을 바꾸기 위해서  App config 만 바꾸자.
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(memberRepository());
+        return new MemberServiceImpl(memberRepository(),discountPolicy());
     }
 
     //Memory에서 DB로 바꾸면 여기만 바꾸면됨.
@@ -28,7 +30,8 @@ public class AppConfig {
 
     public DiscountPolicy discountPolicy()
     {
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,14 +1,18 @@
 package hello.core.member;
 
+import hello.core.discount.DiscountPolicy;
+
 //구현체가 하나일 경우 Impl 용어를 쓴다.
 public class MemberServiceImpl implements MemberService {
 
     //MemberServiceImpl은 추상화에도 의존하고 구현체도 의존한다. MemberRepository , MemoryMemberRepository  모두 의존.
     //추상화에서만 의존한다. 구현은 제대로 모른다.
     private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
+    public MemberServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
