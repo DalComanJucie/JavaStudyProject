0a763c4b941f3824f5884fe749ad023a0d13b13b
DalComanJucie <samjin123kr@gmail.com>
2022-02-18 23:05:21 +0900

    [Study] Spring 으로 전환
    기본 DI/SOLID에 벗어나지 않던 코드를, Spring 에서 지원해주는 애들로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,25 +9,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //생성자를 통해서 주입한다.
 //의존관계 주입한다 Dependency Injection
 //디자인 패턴중 생성 패턴이네... 설정파일 이라는거잖아 ... ㄷ -> // App Config 를 통해서 사용영역과 구성 영역을 나누어 버렸다.
 //구성을 바꾸기 위해서  App config 만 바꾸자.
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository(),discountPolicy());
     }
 
     //Memory에서 DB로 바꾸면 여기만 바꾸면됨.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy()
     {
         //return new FixDiscountPolicy();
